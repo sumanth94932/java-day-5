f718354f5ca1735f0e3d1f9b6ddd4003cbd71a36
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-07 09:36:38 +0530

    Add files via upload

---
+++ patsq.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class patsq{
+    public static void main(String args[]){
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                System.out.print((char)(j+64)+" ");
+            }
+             System.out.println();
+        }
+       
+    }
+}
+
+
+
+       
